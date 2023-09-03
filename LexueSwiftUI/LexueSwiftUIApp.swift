dcb97657698a2b911f21b9135ee843c1d48658ff
BobH <1551608379@qq.com>
2023-09-03 17:47:24 +0800

    Initial Commit

---
+++ LexueSwiftUI/LexueSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexueSwiftUIApp.swift
+//  LexueSwiftUI
+//
+//  Created by bobh on 2023/9/3.
+//
+
+import SwiftUI
+
+@main
+struct LexueSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
