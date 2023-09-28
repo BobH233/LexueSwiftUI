1eef7c3edf29b6418d81f10c6296e17e49184e5f
BobH <1551608379@qq.com>
2023-09-28 14:32:03 +0800

    测试小组件刷星

--- LexueWidget/Provider.swift
+++ LexueWidget/Provider.swift
@@ -6,6 +6,7 @@
 //
 
 import WidgetKit
+import UserNotifications
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> DefaultEntry {
@@ -21,6 +22,24 @@ struct Provider: TimelineProvider {
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<DefaultEntry>) -> ()) {
         print("getTimeline")
+        let identifier = UUID().uuidString
+        let content = UNMutableNotificationContent()
+        content.title = "\(Date.now)"
+        content.body = "widget info!"
+        content.userInfo = ["123":"1234"]
+        content.sound = .default
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(0.01), repeats: false)
+        let request = UNNotificationRequest(
+            identifier: identifier,
+            content: content,
+            trigger: trigger
+        )
+        UNUserNotificationCenter.current().add(request) { error in
+            if error == nil {
+                // print("消息通知已设定: \(identifier)")
+            }
+        }
+        
         let entry = DefaultEntry(date: .now, str: "lalallala2")
         let timeline = Timeline(entries: [entry], policy: .after(.now.advanced(by: 60)))
         completion(timeline)
