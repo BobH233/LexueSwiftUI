24e953bf298df6ee6b91f60b6997d62c799b4dac
BobH <1551608379@qq.com>
2023-09-28 21:19:18 +0800

    集成友盟的sdk到项目中

---
+++ LexueSwiftUI/UMSwift/Bridge.h
@@ -0,0 +1,30 @@
+//
+//  Bridge.h
+//  LexueSwiftUI
+//
+//  Created by bobh on 2023/9/28.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+//导入UMCommon的OC的头文件
+#import <UMCommon/UMCommon.h>
+//导入UMAPM的OC的头文件
+#import <UMAPM/UMLaunch.h>
+#import <UMAPM/UMCrashConfigure.h>
+#import <UMAPM/UMAPMConfig.h>
+
+//导入UMCommonLog的OC的头文件
+#import <UMCommonLog/UMCommonLogManager.h>
+
+//导入UMCommon的OC的头文件
+#import <UMCommon/UMConfigure.h>
+
+//导入UMAnalytics的OC的头文件
+#import <UMCommon/MobClick.h>
+
+//导入UMAnalytics Game的OC的头文件
+#import <UMAnalyticsGame/MobClickGameAnalytics.h>
+
+#endif /* Bridge_h */
