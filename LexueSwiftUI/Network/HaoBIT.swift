1cec80dfb773233c3fdb0919f9ab8c5c029d8065
BobH <1551608379@qq.com>
2023-10-06 19:36:03 +0800

    修复解析乱码的bug

--- LexueSwiftUI/Network/HaoBIT.swift
+++ LexueSwiftUI/Network/HaoBIT.swift
@@ -16,7 +16,6 @@ class HaoBIT {
     let headers = [
         "Referer": "https://haobit.top/",
         "Host": "haobit.top",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0 LexueHelper/114514"
     ]
     
@@ -33,25 +32,34 @@ class HaoBIT {
     }
     func GetNotices() async -> [Notice] {
         var ret = [Notice]()
-        let response = await AF.requestWithoutCache(HAOBIT_FETCH_NOTICE, method: .get, headers: HTTPHeaders(headers)).serializingString().response
-        switch response.result {
-        case .success(let jsonData):
-            if let data = jsonData.data(using: .utf8), let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
-                for notice in json {
-                    var currentNotice = Notice()
-                    currentNotice.link = notice["link"] as? String
-                    currentNotice.title = notice["title"] as? String
-                    currentNotice.source = notice["source"] as? String
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-                    if let dateString = notice["date"] as? String, let date = dateFormatter.date(from: dateString) {
-                        currentNotice.date = date
+        
+        let retJson = await withCheckedContinuation { continuation in
+            AF.requestWithoutCache(HAOBIT_FETCH_NOTICE, method: .get, headers: HTTPHeaders(headers)).response { res in
+                switch res.result {
+                case .success(let data):
+                    if let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? [[String: Any]] {
+                        continuation.resume(returning: json)
+                    } else {
+                        print("无法将响应数据转换为字典")
+                        continuation.resume(returning: [[String: Any]]())
                     }
-                    ret.append(currentNotice)
+                case .failure(_):
+                    print("请求HAOBIT 失败")
+                    continuation.resume(returning: [[String: Any]]())
                 }
             }
-        case .failure(_):
-            print("获取HAOBIT Notices 失败")
+        }
+        for notice in retJson {
+            var currentNotice = Notice()
+            currentNotice.link = notice["link"] as? String
+            currentNotice.title = notice["title"] as? String
+            currentNotice.source = notice["source"] as? String
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+            if let dateString = notice["date"] as? String, let date = dateFormatter.date(from: dateString) {
+                currentNotice.date = date
+            }
+            ret.append(currentNotice)
         }
         return ret
     }
