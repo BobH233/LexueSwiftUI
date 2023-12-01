9aecffc756b1dec9886f7c9a83cd95c3cfdf599a
BobH <1551608379@qq.com>
2023-12-01 23:37:10 +0800

    更改反馈邮箱链接

--- LexueSwiftUI/Views/Setting/Feedback.swift
+++ LexueSwiftUI/Views/Setting/Feedback.swift
@@ -23,7 +23,7 @@ let markdownFeedbackTipsForTestFlight = """
 
 **欢迎您添加意见反馈QQ群：297983455**，提出您宝贵的意见，对“i乐学助手”有帮助的建议我们都会采纳并附上感谢！
 
-您也可以向开发者发送邮件：[bobh@zendee.cn](mailto:bobh@zendee.cn)
+您也可以向开发者发送邮件：[admin@bit-helper.cn](mailto:admin@bit-helper.cn)
 
 """
 
@@ -34,7 +34,7 @@ let markdownFeedbackTipsForCommonUser = """
 
 我们非常乐意倾听您对于“i乐学助手”的建议，也欢迎您提出功能上的需求。如果您在使用中遇到任何bug，或者对于功能的缺失感到失望，欢迎您联系我们。
 
-欢迎您直接联系开发者[bobh@zendee.cn](mailto:bobh@zendee.cn)，提出您宝贵的意见，对“i乐学助手”有帮助的建议我们都会采纳并附上感谢！
+欢迎您直接联系开发者[admin@bit-helper.cn](mailto:admin@bit-helper.cn)，提出您宝贵的意见，对“i乐学助手”有帮助的建议我们都会采纳并附上感谢！
 
 # 欢迎参与内测
 
